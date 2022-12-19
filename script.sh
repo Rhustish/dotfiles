2ad303269ba4b3f8c1c738dd7351401461134cb3
Bhavy Bhatnagar <bbhatnagar27@gmail.com>
2022-12-19 17:09:17 +0530

    started work on script

---
+++ script.sh
@@ -0,0 +1,7 @@
+sudo pacman -S nano git htop neofetch make feh ttf-dejavu alacritty xorg xorg-xinit i3-gaps pulseaudio-bluetooth
+git clone https://aur.archlinux.org/yay.git
+cd yay/
+makepkg -si
+cd
+
+yay -S visual-studio-code-bin blueberry google-chrome 
