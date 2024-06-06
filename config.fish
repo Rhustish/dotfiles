efe88bdd8c54874334cd8a12b4c212e038210758
Bhavy Bhatnagar <bbhatnagar27@gmail.com>
2024-06-06 20:17:22 +0530

    kitty and fist

--- config.fish
+++ config.fish
@@ -21,6 +21,7 @@ abbr .5 'cd ../../../../..'
 # Always mkdir a path (this doesn't inhibit functionality to make a single dir)
 abbr mkdir 'mkdir -p'
 abbr pacman 'sudo pacman'
+abbr purge "sudo pacman -Sc && yay -Yc && paccache -rk0 && rm -fr ~/.cache/yay/*"
 
 #user
 
