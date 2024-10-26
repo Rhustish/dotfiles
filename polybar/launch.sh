4f0a7bc4918dae8406b52bde0b85e486f997c0ed
rhustish <bbhatnagar27@gmail.com>
2024-10-26 12:06:27 +0530

    migrating,  yeah, again

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -10,7 +10,7 @@ killall -q polybar
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar one 2>&1 | tee -a /tmp/polybar1.log &
-polybar two 2>&1 | tee -a /tmp/polybar1.log &
+#polybar two 2>&1 | tee -a /tmp/polybar1.log &
 disown
 
 echo "Bars launched..."
