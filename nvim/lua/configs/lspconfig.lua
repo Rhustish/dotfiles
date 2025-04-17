7f0c6211aad743d811c43922a92ecce1b06e69b5
Bhavy Bhatnagar <bbhatnagar@ea.com>
2025-04-17 16:04:27 +0530

    nvim changes

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -4,10 +4,9 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" , "clangd" ,"ts_ls" , "eslint"}
+local servers = {"omnisharp", "html", "cssls" , "clangd" ,"ts_ls" , "eslint" , "denols"}
 
 local nvlsp = require "nvchad.configs.lspconfig"
-
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -17,9 +16,43 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+local nvim_lsp = require('lspconfig')
+nvim_lsp.denols.setup {
+  root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+}
+  lspconfig.omnisharp.setup {
+      cmd = { "dotnet", "/Users/bbhatnagar/omnisharp-roslyn/bin/Release/OmniSharp.Stdio.Driver/net6.0/OmniSharp.dll" },
+
+      settings = {
+        FormattingOptions = {
+          EnableEditorConfigSupport = true,
+          OrganizeImports = true,
+        },
+        MsBuild = {
+          LoadProjectsOnDemand = nil,
+        },
+        RoslynExtensionsOptions = {
+          EnableAnalyzersSupport = true,
+          EnableImportCompletion = true,
+          AnalyzeOpenDocumentsOnly = nil,
+          DiagnosticWorkersThreadCount = 8,
+        },
+        InlayHintsOptions = {
+          EnableForParameters = true,
+          ForLiteralParameters = true,
+          ForIndexerParameters = true,
+          ForObjectCreationParameters = true,
+          ForOtherParameters = true,
+          SuppressForParametersThatDifferOnlyBySuffix = false,
+          SuppressForParametersThatMatchMethodIntent = false,
+          SuppressForParametersThatMatchArgumentName = false,
+          EnableForTypes = true,
+          ForImplicitVariableTypes = true,
+          ForLambdaParameterTypes = true,
+          ForImplicitObjectCreation = true
+        },
+        Sdk = {
+          IncludePrereleases = true,
+        },
+      },
+  }
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -12,15 +12,67 @@ return {
       require "configs.lspconfig"
     end,
   },
-
   {
   	"nvim-treesitter/nvim-treesitter",
   	opts = {
   		ensure_installed = {
   			"vim", "lua", "vimdoc",
        "html", "css", "cpp"
-        ,"typescript", "javascript"
+        ,"typescript", "javascript",
+        "c_sharp"
   		},
   	},
+  },
+{
+  "github/copilot.vim",
+  lazy = false,
+  config = function()  -- Mapping tab is already used by NvChad
+    vim.g.copilot_no_tab_map = true;
+    vim.g.copilot_assume_mapped = true;
+    vim.g.copilot_tab_fallback = "";
+  -- The mapping is set to other key, see custom/lua/mappings
+  -- or run <leader>ch to see copilot mapping section
+  end
+},
+  {
+    'abecodes/tabout.nvim',
+    lazy = false,
+    config = function()
+      require('tabout').setup {
+        tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
+        backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
+        act_as_tab = true, -- shift content if tab out is not possible
+        act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+        default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+        default_shift_tab = '<C-d>', -- reverse shift default action,
+        enable_backwards = true, -- well ...
+        completion = false, -- if the tabkey is used in a completion pum
+        tabouts = {
+          { open = "'", close = "'" },
+          { open = '"', close = '"' },
+          { open = '`', close = '`' },
+          { open = '(', close = ')' },
+          { open = '[', close = ']' },
+          { open = '{', close = '}' }
+        },
+        ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+        exclude = {} -- tabout will ignore these filetypes
+      }
+    end,
+    dependencies = { -- These are optional
+      "nvim-treesitter/nvim-treesitter",
+      "L3MON4D3/LuaSnip",
+      "hrsh7th/nvim-cmp"
+    },
+    opt = true,  -- Set this to true if the plugin is optional
+    event = 'InsertCharPre', -- Set the event to 'InsertCharPre' for better compatibility
+    priority = 1000,
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      -- Disable default tab keybinding in LuaSnip
+      return {}
+    end,
   },
 }
